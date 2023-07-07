9e851627ed80bdbc13d2d5846323773b051425fb
Vasanthakumar K <104672066+amvasanthsurya@users.noreply.github.com>
2023-07-07 22:48:35 +0530

    main.py

--- week1-tasks/main.py
+++ week1-tasks/main.py
@@ -1,94 +1,81 @@
-def add_contact(contacts, name, phone):
-    contacts[name] = phone
-    print(f"{name} has been added to contacts.")
-def search_contact(contacts):
-    for i, j in contacts.items():
-        print('Name:', i, 'Number:', j)
-def update_contact(contacts):
-    name = input('Enter the contact to be edited')
-    if name in contacts:
-        phone = input('Enter the new number:')
-        contacts[name] = phone
-        print('contact updated')
-        search_contact(contacts)
+CONTACTS_FILE = "contacts.txt"
+def add_contact(contacts, name, phone_number):
+    if len(phone_number) == 10:
+        contacts[name] = phone_number
+        print(f"{name} has been added to contacts.")
     else:
-        print('The name is not found in contact book')
+        print("Invalid phone number. Please enter a 10-digit number.")
+def search_contact(contacts, name):
+    if name in contacts:
+        print(f"Name: {name}\tNumber: {contacts[name]}")
+    else:
+        print("Contact not found.")
+def update_contact(contacts, name, new_phone_number):
+    if name in contacts:
+        contacts[name] = new_phone_number
+        print(f"{name}'s contact information has been updated.")
+        search_contact(contacts, name)
+    else:
+        print("Contact not found.")
 def delete_contact(contacts, name):
     if name in contacts:
         del contacts[name]
         print(f"{name} has been deleted from contacts.")
     else:
-        print(f"{name} not found in contacts.")
-def list_contacts(contacts):
+        print("Contact not found.")
+def view_contacts(contacts):
     if not contacts:
         print("No contacts found.")
     else:
         print("Contacts:")
-        for name, contact in contacts.items():
-            print('Name:',name,'\tNumber:',contact)
-def save_contacts(contacts, filename):
-    with open(filename, 'w') as file:
-        for name, contact in contacts.items():
-            file.write(f"{name},{contact}\n")
-def load_contacts(filename):
+        for name, phone_number in contacts.items():
+            print(f"Name: {name}\tNumber: {phone_number}")
+def save_contacts(contacts):
+    with open(CONTACTS_FILE, 'w') as file:
+        for name, phone_number in contacts.items():
+            file.write(f"{name},{phone_number}\n")
+def load_contacts():
     contacts = {}
-    try:
-        with open(filename, 'r') as file:
-            lines = file.readlines()
-            for line in lines:
-                line = line.strip()
-                if line.count(',') == 1:
-                    name, phone = line.split(',')
-                    contacts[name] = phone
-                else:
-                    print(f"Ignoring invalid line: {line}")
-    except FileNotFoundError:
-        pass
+    file = open(CONTACTS_FILE, 'r')
+    lines = file.readlines()
+    file.close()
+    for line in lines:
+        name, phone_number = line.strip().split(',')
+        contacts[name] = phone_number
     return contacts
 
-def main():
-    filename = "contacts.txt"
-    contacts = load_contacts(filename)
+contacts = load_contacts()
 
-    while True:
-        print("\nContact Management System")
-        print("1. Add Contact")
-        print("2. Search Contact")
-        print("3. Update Contact")
-        print("4. Delete Contact")
-        print("5. List Contacts")
-        print("6. Save and Exit")
+while True:
+    print("\nContact Management System")
+    print("1. Add Contact")
+    print("2. Search Contact")
+    print("3. Update Contact")
+    print("4. Delete Contact")
+    print("5. View Contacts")
+    print("6. Save and Exit")
 
-        choice = input("Enter your choice (1-6): ")
+    choice = input("Enter your choice (1-6): ")
 
-        if choice == '1':
-            name = input("Enter the name: ")
-            phone = input("Enter the phone number: ")
-            if len(phone) == 10 and phone.isdigit():
-                add_contact(contacts,name,phone)
-            else:
-                print("Invalid phone number. Please enter a 10-digit number.")
-
-
-        elif choice == '2':
-            Search_contact = input('Enter the name')
-            if Search_contact in contacts:
-                print('Name :', Search_contact, '\nContact number:', contacts[Search_contact])
-            else:
-                print('The name is not found in contact book')
-        elif choice=='3':
-            update_contact(contacts)
-        elif choice == '4':
-            name = input("Enter the name: ")
-            delete_contact(contacts, name)
-        elif choice == '5':
-            list_contacts(contacts)
-        elif choice == '6':
-            save_contacts(contacts, filename)
-            print("Contacts saved successfully. Exiting...")
-            break
-        else:
-            print("Invalid choice. Please try again.")
-
-if __name__ == "__main__":
-    main()
+    if choice == '1':
+        name = input("Enter the name: ")
+        phone_number = input("Enter the phone number: ")
+        add_contact(contacts, name, phone_number)
+    elif choice == '2':
+        name = input("Enter the name: ")
+        search_contact(contacts, name)
+    elif choice == '3':
+        name = input("Enter the name: ")
+        new_phone_number = input("Enter the new phone number: ")
+        update_contact(contacts, name, new_phone_number)
+    elif choice == '4':
+        name = input("Enter the name: ")
+        delete_contact(contacts, name)
+    elif choice == '5':
+        view_contacts(contacts)
+    elif choice == '6':
+        save_contacts(contacts)
+        print("Contacts saved successfully. Exiting...")
+        break
+    else:
+        print("Invalid choice. Please try again.")
